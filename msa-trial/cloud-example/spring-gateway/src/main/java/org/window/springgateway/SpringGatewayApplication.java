243ee4c22c8fd994cabc4883b26a941cde14dd66
Jivebread <spark@troy.edu>
2024-11-11 21:03:18 +0900

    - Spring gateway traceId inserted

--- msa-trial/cloud-example/spring-gateway/src/main/java/org/window/springgateway/SpringGatewayApplication.java
+++ msa-trial/cloud-example/spring-gateway/src/main/java/org/window/springgateway/SpringGatewayApplication.java
@@ -2,11 +2,14 @@ package org.window.springgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Hooks;
 
 @SpringBootApplication
 public class SpringGatewayApplication {
 
   public static void main(String[] args) {
+    Hooks.enableAutomaticContextPropagation();
+
     SpringApplication.run(SpringGatewayApplication.class, args);
   }
 
