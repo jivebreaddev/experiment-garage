65b874d5b241810853ece8497f3d756803acb311
Jivebread <spark@troy.edu>
2024-12-03 21:30:12 +0900

    feature: archunit 과 jacoco를 멀티모듈을 위해 도입합니다.

---
+++ msa-trial/cloud-example/support/archunit/src/test/java/org/window/archunit/ArchunitApplicationTests.java
@@ -0,0 +1,26 @@
+package org.window.archunit;
+
+import com.tngtech.archunit.lang.syntax.ArchRuleDefinition;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchunitApplicationTests {
+
+  // ??? ?????? ??? ??? ? ??.
+  @Test
+  void enforceModuleDependencyRules() {
+    //    ArchRuleDefinition.classes()
+    //        .that().resideInAPackage("com.example.project.moduleA..")
+    //        .should().onlyAccessClassesThat()
+    //        .resideInAnyPackage("com.example.project.moduleA..",
+    //            "java..") // Allow standard Java dependencies
+    //        .check(importedClasses);
+    //
+    //    ArchRuleDefinition.noClasses()
+    //        .that().resideInAPackage("com.example.project.moduleB..")
+    //        .should().dependOnClassesThat()
+    //        .resideInAPackage("com.example.project.moduleC..")
+    //        .check(importedClasses);
+    //  }
+}
